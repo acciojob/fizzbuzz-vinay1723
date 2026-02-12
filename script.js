8aa354e93bd1250055445591be32cc3296049481
Vinay Kumar Goud <144577819+vinay1723@users.noreply.github.com>
2026-02-12 12:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+function answer() {
+	for (let index = 1; index <=100; index++){
+		if (index % 3 === 0 && index % 5 === 0) {
+			alert('FizzBuzz');
+			
+		}else if(index % 3 === 0) {
+			alert('Fizz');
+		
+		}else if(index % 5 === 0) {
+			alert('Buzz');
+		}else{
+			alert(index);
+		}
+	}
+}
+
+answer();
